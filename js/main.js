137f47c5b2380b1b0cb445e84eadec8d2bb85bf7
ahshasib <154332574+ahshasib@users.noreply.github.com>
2025-03-02 01:06:57 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,70 @@
+let colorBtn = document.getElementById("colorChange");
+
+if(colorBtn){
+    colorBtn.addEventListener("click", function(){
+        const randomCod = Math.floor(Math.random() * 16777215);
+        const makeColor = "#" + randomCod.toString(16);
+    
+        document.body.style.backgroundColor = makeColor;
+    });
+}
+
+
+
+let completBtn = document.querySelectorAll(".complet");
+
+completBtn.forEach(function(button){
+    button.addEventListener("click", function(){
+        let increaseNumber = document.querySelector(".increaseNumber");
+        let intIncreaseNum = parseInt(increaseNumber.innerText);
+        let decreaseNumber = document.querySelector(".decreaseNumber");
+        let intnumber = parseInt(decreaseNumber.innerText);
+        let histroy = document.querySelector(".histroy");
+        let title = this.closest(".parentdiv").querySelector(".title").innerText;
+        const clearBtn = document.querySelector(".clearBtn");
+
+        alert()
+        intnumber --;
+        intIncreaseNum ++;
+        increaseNumber.innerText = intIncreaseNum;
+        decreaseNumber.innerText = intnumber;
+        this.disabled = true;
+
+
+        // history functionality start
+        let makeP = document.createElement("p");
+        makeP.innerText = `Completed: ${title} ${bdTime}`;
+        makeP.classList.add("text-lg", "p-4", "bg-white", "rounded", "m-4");
+        
+        histroy.appendChild(makeP);
+        console.log(title)
+
+
+        clearBtn.addEventListener("click", function(){
+            histroy.innerHTML = "";
+        });
+    
+
+
+    })
+
+   
+});
+
+  let discoverBtn = document.querySelector(".discoverbtn");
+  if(discoverBtn){
+    discoverBtn.addEventListener("click", function(){
+        window.location.href= "./another.html" ;
+    
+    
+      })
+    
+  }
+  
+const backToDesk = document.getElementById("backToDesk");
+if (backToDesk) {
+backToDesk.addEventListener("click", function () {
+    window.location.href = "index.html";
+});
+   
+}
---
+++ js/utility.js
@@ -0,0 +1,6 @@
+let bdTime = new Date().toLocaleTimeString("en-US", { 
+    timeZone: "Asia/Dhaka", 
+    hour: "2-digit", 
+    minute: "2-digit", 
+    hour12: true 
+});
